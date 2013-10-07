52fa8f3c41a7208c27838367cc4ddb40b39cf937
boui <rocksyseletska2837@gmail.com>
2013-10-07 05:12:18 +0300

    filters reviewed

--- public/js/contacts/controllers.js
+++ public/js/contacts/controllers.js
@@ -18,8 +18,8 @@ contactsModule.controller('ContactsCtrl', function ($scope) {
         {
             id: 0,
             name: 'Vasya Petrov',
-            photoUrl:'http://m3.c.lnkd.licdn.com/mpr/mpr/shrink_80_80/p/7/000/1fb/091/15b5b3f.jpg',
-            groups: [2],
+            photoUrl: 'http://m3.c.lnkd.licdn.com/mpr/mpr/shrink_80_80/p/7/000/1fb/091/15b5b3f.jpg',
+            groups: [{title: 'work', id: 2, active: false}],
             contacts: {
                 gmail: [
                     'vasya@gmail.com'
@@ -34,8 +34,11 @@ contactsModule.controller('ContactsCtrl', function ($scope) {
         {
             id: 1,
             name: 'Dart Vaider',
-            photoUrl:'http://m1.c.lnkd.licdn.com/mpr/mpr/shrink_60_60/p/3/000/0bf/12b/25cb8e8.jpg',
-            groups: [0, 1],
+            photoUrl: 'http://m1.c.lnkd.licdn.com/mpr/mpr/shrink_60_60/p/3/000/0bf/12b/25cb8e8.jpg',
+            groups: [
+                {title: 'friends', id: 0, active: false},
+                {title: 'love', id: 1, active: false}
+            ],
             contacts: {
                 gmail: [
                     'vasya@gmail.com'
@@ -58,6 +61,10 @@ contactsModule.controller('DetailsCtrl', function ($scope) {
     $scope.activeContact = null;
     $scope.showDetails = function (contact) {
         $scope.activeContact = contact;
+    }
+
+    $scope.hideDetails = function () {
+        $scope.activeContact = null;
     }
 
 });
--- public/js/filters.js
+++ public/js/filters.js
@@ -3,23 +3,40 @@
 var app = angular.module('contactsApp');
 
 app.filter('inActiveGroups', function () {
-    return function (contacts, groups) {
+    return function (contacts, groups, selectedContact, hideFn, showFn) {
         var activeGroups = _.filter(groups, function (item) {return item.active});
         if (_.isEmpty(activeGroups)) return contacts;
 
         var filtered = _.filter(contacts, function(c){
-                    return _.intersection(c.groups, _.map(activeGroups,function(group){return group.id})).length > 0
+
+            return _.intersection(
+                        _.map(c.groups, function(group){return group.id}),
+                        _.map(activeGroups,function(group){return group.id}))
+            .length > 0
         });
 
-        return _.isEmpty(filtered) ? contacts:filtered;
+        var result = _.isEmpty(filtered) ? contacts:filtered;
+        if(selectedContact && !_.contains(result, selectedContact)) hideFn();
+        if(selectedContact && result.length == 1) showFn(result[0]);
+
+        return result;
     }
 });
 
 app.filter('validDueToSearchInput', function(){
-    return function(contacts, searchText){
+    return function(contacts, searchText, selectedContact, hideFn, showFn){
         var patt = new RegExp(searchText,"i");
-        return !!searchText ? _.filter(contacts, function(c){
-            return !_.isEmpty(c.name.toUpperCase().match(patt));
-        }):contacts;
+
+        var result = null;
+        if(!!searchText) {
+            result = _.filter(contacts, function(c){
+                return !_.isEmpty(c.name.toUpperCase().match(patt));
+            })
+        } else{ result = contacts };
+
+        if(selectedContact && !_.contains(_.map(result, function(c){return c.id}), selectedContact.id)) hideFn();
+        if(result.length == 1) showFn(result[0]);
+
+        return result;
     }
 })
