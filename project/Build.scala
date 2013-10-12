4113c4d40dfc83eec8a278f8c0373edb5e4bc033
boui <rocksyseletska2837@gmail.com>
2013-10-13 02:26:51 +0300

    refactoring for scalatra

--- project/Build.scala
+++ project/Build.scala
@@ -1,15 +1,47 @@
 import sbt._
-import play.Project._
+import Keys._
+import com.mojolly.scalate.ScalatePlugin._
+import ScalateKeys._
+import com.earldouglas.xsbtwebplugin.WebPlugin.webSettings
+
+
 
 object ApplicationBuild extends Build {
 
-  val appName = "angular-seed-play"
+  val appName = "doujs_test"
   val appVersion = "1.0-SNAPSHOT"
+  val ScalatraVersion = "2.2.1"
+  val ScalaVersion = "2.10"
 
-  val appDependencies = Seq(
-    "org.webjars" % "angularjs" % "1.1.5-1",
-    "org.webjars" % "requirejs" % "2.1.1",
-    "org.webjars" %% "webjars-play" % "2.1.0-3")
+  val appDependencies = Seq()
 
-  val main = play.Project(appName, appVersion, appDependencies)
+  lazy val project = Project (
+  "doujs_test",
+    file("."),
+    settings = Defaults.defaultSettings ++ scalateSettings++ webSettings ++ Seq(
+      name := appName,
+      version := appVersion,
+      scalaVersion := ScalaVersion,
+      resolvers += Classpaths.typesafeReleases,
+      libraryDependencies ++= Seq(
+        "org.scalatra" %% "scalatra" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+        "org.scalatra" %% "scalatra-json" % ScalatraVersion,
+        "org.json4s"   %% "json4s-jackson" % "3.2.4",
+        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test"
+      ),
+      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
+        Seq(
+          TemplateConfig(
+            base / "webapp" / "WEB-INF" / "templates",
+            Seq.empty,  /* default imports should be added here */
+            Seq(
+              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
+            ),  /* add extra bindings here */
+            Some("templates")
+          )
+        )
+      }
+    )
+  )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,8 +1,5 @@
-// Comment to get more information during initialization
 logLevel := Level.Warn
 
-// The Typesafe repository
-resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+resolvers += Resolver.sonatypeRepo("releases")
 
-// Use the Play sbt plugin for Play projects
-addSbtPlugin("play" % "sbt-plugin" % "2.1.3")
+addSbtPlugin("org.scalatra.sbt" % "scalatra-sbt" % "0.3.2")
